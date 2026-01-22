65208a7296d8591f5fd7bb4b1517171709424fac
gearkid1127 <gearkid1127@gmail.com>
2026-01-22 09:59:43 -0600

    Created enter and testing routes

---
+++ app/enter/page.tsx
@@ -0,0 +1,7 @@
+export default function EnterPage() {
+  return (
+    <main>
+      <h1>Phase 1</h1>
+    </main>
+  );
+}
---
+++ app/testing/page.tsx
@@ -0,0 +1,7 @@
+export default function TestingPage() {
+  return (
+    <main>
+      <h1>To start analysis</h1>
+    </main>
+  );
+}
